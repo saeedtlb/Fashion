559d1663d7a877402b6d2dabad62a245dc31c0b9
saeed-tlb <saeedt7879s@gmail.com>
2020-10-13 15:57:09 +0330

    create folder for components and start codeing the htmls(actually jsx)

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            <div className='navbar__menu'></div>
+            <div className='navbar__lang'>eng</div>
+            <div className='navbar__search'></div>
+        </nav>
+    );
+};
+
+export default Navbar;
