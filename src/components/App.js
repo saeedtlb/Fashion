6f734ed2f185367f68536f99d0aac3e3e3c62d56
saeed-tlb <saeedt7879s@gmail.com>
2020-10-13 21:25:37 +0330

    animate the brand name in page(fade it in after just a little short amount of time)

--- src/components/App.js
+++ src/components/App.js
@@ -6,15 +6,16 @@ import Navbar from './Navbar';
 import Social from './Social';
 import Sponsor from './Sponsor';
 import Text from './Text';
+import Distortion from './Distortion';
 
 const App = () => {
-    console.log('come');
     return (
         <div className='App'>
             <Navbar />
             <Social />
-            <Sponsor />
             <Text />
+            <Sponsor />
+            <Distortion />
         </div>
     );
 };
--- src/components/Text.js
+++ src/components/Text.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import '../Resources/Css/text.css';
 
+import { Expo, TweenMax } from 'gsap';
+
 const Text = () => {
+    const brand_ref = useRef(null);
+
+    useEffect(() => {
+        TweenMax.from(brand_ref.current, 1.5, {
+            delay: 0.7,
+            y: '100%',
+            ease: Expo.easeInOut,
+        });
+    }, []);
+
     return (
         <article className='text'>
-            <h1>toni&amp;guy</h1>
+            <h1>
+                <span className='hidetext' ref={brand_ref}>
+                    toni&amp;guy
+                </span>
+            </h1>
+            <h2>duality</h2>
             <h3>
-                collection 2020 <br /> duality
+                <span className='hidetext'>
+                    collection 2020 <br /> duality
+                </span>
             </h3>
             <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
-                molestiae vitae nulla nemo unde, dolor dignissimos deserunt
-                impedit optio ullam neque. Perspiciatis, repellat. Rem illum
-                voluptatibus quibusdam laborum. Autem, vero?
+                <span className='hidetext'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Enim molestiae vitae nulla nemo unde, dolor dignissimos
+                    deserunt impedit optio ullam neque. Perspiciatis, repellat.
+                    Rem illum voluptatibus quibusdam laborum. Autem, vero?
+                </span>
             </p>
         </article>
     );
