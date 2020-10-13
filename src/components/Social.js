25cec68443f32b3739e4c8bfdb0de0369035293a
saeed-tlb <saeedt7879s@gmail.com>
2020-10-13 16:08:09 +0330

    finish initial html

---
+++ src/components/Social.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Social = () => {
+    return (
+        <div>
+            <div className='social_media'>
+                <ul>
+                    <li>facebook</li>
+                    <li>instagram</li>
+                    <li>twitter</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Social;
