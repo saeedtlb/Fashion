f7178d3d91805ca944f5bd8c29c9da2330464c4f
saeed-tlb <saeedt7879s@gmail.com>
2020-10-13 17:29:22 +0330

    text component which has main texts

---
+++ src/components/Text.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import '../Resources/Css/text.css';
+
+const Text = () => {
+    return (
+        <article className='text'>
+            <h1>toni&amp;guy</h1>
+            <h3>
+                collection 2020 <br /> duality
+            </h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim
+                molestiae vitae nulla nemo unde, dolor dignissimos deserunt
+                impedit optio ullam neque. Perspiciatis, repellat. Rem illum
+                voluptatibus quibusdam laborum. Autem, vero?
+            </p>
+        </article>
+    );
+};
+
+export default Text;
