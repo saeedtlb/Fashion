f71af35f6f52f2f4e3fc84f5b0005222e724458a
saeed-tlb <saeedt7879s@gmail.com>
2020-10-13 17:27:09 +0330

    create a folder for utils and for yet add hamburger component in it

---
+++ src/utils/Hamburger.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import '../Resources/Css/hamburger.css';
+
+const Hamburger = () => {
+    return (
+        <div className='hamburger'>
+            <label>
+                <input type='checkbox' />
+                <svg viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
+                    <circle cx='50' cy='50' r='30' />
+                    <path
+                        className='line--1'
+                        d='M0 40h62c13 0 6 28-4 18L35 35'
+                    />
+                    <path className='line--2' d='M0 50h70' />
+                    <path
+                        className='line--3'
+                        d='M0 60h62c13 0 6-28-4-18L35 65'
+                    />
+                </svg>
+            </label>
+        </div>
+    );
+};
+
+export default Hamburger;
