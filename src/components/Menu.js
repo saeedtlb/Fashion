b2794a46c75876a480d4d778e8613b4d65292f24
saeed-tlb <saeedt7879s@gmail.com>
2020-10-14 19:44:21 +0330

    write all functionality, refs, animation of menu here

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,16 +1,49 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 import '../Resources/Css/menu.css';
 
-const Menu = () => {
+import { TimelineMax, Expo } from 'gsap';
+import { addToRef } from '../utils/misc';
+
+const Menu = ({ show }) => {
+    const below_ref = useRef(null);
+    const menu_ref = useRef(null);
+    const items_ref = useRef([]);
+    const tl = useRef(new TimelineMax({ paused: true }));
+
+    useEffect(() => {
+        if (show === null) return;
+        show ? tl.current.play() : tl.current.reverse();
+    }, [show]);
+
+    useEffect(() => {
+        tl.current
+            .to(below_ref.current, { duration: 0.4, left: '-98%' })
+            .to(below_ref.current, { duration: 0.3, height: '100%' })
+            .to(below_ref.current, { duration: 0.8, left: 0 })
+            .to(menu_ref.current, { duration: 0.9, left: 0 }, '-=0.7')
+            .set(items_ref.current, { display: 'block' })
+            .staggerTo(
+                items_ref.current,
+                0.6,
+                { scale: 1, opacity: 1, rotate: '0deg', ease: Expo.easeOut },
+                0.25
+            );
+    }, []);
+
     return (
-        <div className='menu'>
-            <ul>
-                <li>main</li>
-                <li>products</li>
-                <li>info</li>
-                <li>contact</li>
-            </ul>
+        <div className='menu_wrapper'>
+            <div className='below' ref={below_ref}></div>
+            <div className='menu' ref={menu_ref}>
+                <ul>
+                    <li ref={el => addToRef(el, items_ref.current)}>main</li>
+                    <li ref={el => addToRef(el, items_ref.current)}>
+                        products
+                    </li>
+                    <li ref={el => addToRef(el, items_ref.current)}>info</li>
+                    <li ref={el => addToRef(el, items_ref.current)}>contact</li>
+                </ul>
+            </div>
         </div>
     );
 };
