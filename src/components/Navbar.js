24af7bbe4144c6b6a5b560c8b5f51ace4133a9c0
saeed-tlb <saeedt7879s@gmail.com>
2020-10-14 19:41:56 +0330

    define function which passed to hamburger menu and send circle fill color

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,13 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 import '../Resources/Css/navbar.css';
 
 import Hamburger from '../utils/Hamburger';
+import Menu from './Menu';
 import { addToRef, animationStagger } from '../utils/misc';
 
 const Navbar = () => {
+    const [menu, setMenu] = useState(null);
     const navbar_ref = useRef([]);
 
     useEffect(() => {
@@ -16,21 +18,28 @@ const Navbar = () => {
     }, []);
 
     return (
-        <nav className='navbar'>
-            <Hamburger navbar={navbar_ref.current} />
-            <div
-                className='navbar__lang'
-                ref={el => addToRef(el, navbar_ref.current)}
-            >
-                eng
-            </div>
-            <div
-                className='navbar__search'
-                ref={el => addToRef(el, navbar_ref.current)}
-            >
-                <img src='/images/search.png' alt='search' />
-            </div>
-        </nav>
+        <>
+            <nav className='navbar'>
+                <Hamburger
+                    navbar={navbar_ref.current}
+                    click={() => setMenu(prev => !prev)}
+                    color={menu ? 'tomato' : 'gold'}
+                />
+                <div
+                    className='navbar__lang'
+                    ref={el => addToRef(el, navbar_ref.current)}
+                >
+                    eng
+                </div>
+                <div
+                    className='navbar__search'
+                    ref={el => addToRef(el, navbar_ref.current)}
+                >
+                    <img src='/images/search.png' alt='search' />
+                </div>
+            </nav>
+            <Menu show={menu} />
+        </>
     );
 };
 
