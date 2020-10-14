ce2bd5133e4846e99de39a364a491ffd817e2fa2
saeed-tlb <saeedt7879s@gmail.com>
2020-10-14 19:40:43 +0330

    add onclick event to burger menu and make the circle fill be dynamic

--- src/utils/Hamburger.js
+++ src/utils/Hamburger.js
@@ -4,13 +4,17 @@ import '../Resources/Css/hamburger.css';
 
 import { addToRef } from './misc';
 
-const Hamburger = ({ navbar }) => {
+const Hamburger = ({ navbar, click, color }) => {
     return (
         <div className='hamburger' ref={el => addToRef(el, navbar)}>
             <label>
                 <input type='checkbox' />
-                <svg viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
-                    <circle cx='50' cy='50' r='30' />
+                <svg
+                    viewBox='0 0 100 100'
+                    xmlns='http://www.w3.org/2000/svg'
+                    onClick={click}
+                >
+                    <circle cx='50' cy='50' r='30' fill={color} />
                     <path
                         className='line--1'
                         d='M0 40h62c13 0 6 28-4 18L35 35'
